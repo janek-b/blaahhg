63e524a218f6d81d3ada257ebeb16929d2870fd1
Dallas Slaughter and Janek Brandt <janek@>
2017-04-26 15:02:16 -0700

    add query to find user in db based on auth uid

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
 
+export default Ember.Route.extend({
   beforeModel: function() {
     return this.get('session').fetch().catch(function() {
     });
@@ -11,13 +11,25 @@ export default Ember.Route.extend({
     signIn: function(provider) {
       this.get('session').open('firebase', {
         provider: provider,
-      }).then(function(data) {
+      }).then(data => {
         console.log(data.currentUser);
+        var userID = data.currentUser.uid;
+        // var storedUser = this.get('store').findRecord('user', 0).then(result => console.log(result.toJSON().name));
+        var storedUser = this.get('store').query('user', {
+          filter: {
+            'userID': userID
+          }}).then(users => {
+            return users.get('firstObject');
+          }).then(result => console.log(result.toJSON()));
+        // console.log(storedUser);
+        // console.log(this.get('session').get('currentUser'))
+        // console.log(this);
       });
     },
 
     signOut: function() {
       this.get('session').close();
     },
+
   },
 });
